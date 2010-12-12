82a92991d0271a95e660f476abb6688a05240322
James Dennes <jdennes@gmail.com>
2010-12-13 09:07:32 +1100

    Adding expand and collapse icons.

--- public/index.js
+++ public/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   $("div[id|=title]").click(function() {
     var gifts = $(this).parent().children("div[id|=gifts]");
+    var icon = $(this).children(".icon");
     if (gifts.is(":visible")) {
       gifts.slideUp();
+      icon.html('<img src="/images/plus.gif" />')
     } else {
       gifts.slideDown();
+      icon.html('<img src="/images/minus.gif" />')
     }
   });
 });
